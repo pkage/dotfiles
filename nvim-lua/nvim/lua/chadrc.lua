08ab22b3a55b5f6561372ed59be9de2440910acd
Patrick Kage <patrick.r.kage@gmail.com>
2025-11-29 21:12:02 -0600

    add theme, lazygit

--- nvim-lua/nvim/lua/chadrc.lua
+++ nvim-lua/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "yoru",
     integrations = { 'hop', 'avante' },
 
 	-- hl_override = {
--- nvim-lua/nvim/lua/plugins/init.lua
+++ nvim-lua/nvim/lua/plugins/init.lua
@@ -22,7 +22,7 @@ return {
     },
 
     -- test new blink
-    -- { import = "nvchad.blink.lazyspec" },
+    { import = "nvchad.blink.lazyspec" },
 
     -- {
     -- 	"nvim-treesitter/nvim-treesitter",
@@ -58,4 +58,26 @@ return {
         'pkage/sparkup',
         ft = {"html", "htmldjango", "njk", "smarty", "svelte", "xml"}
     },
+
+    -- lazygit
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    }
 }
