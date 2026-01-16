c8767782c0b1e7529763160d081cdf1567fb0b42
Patrick Kage <patrick.r.kage@gmail.com>
2026-01-15 20:56:28 -0500

    for now automatically start tree-sitter in python

--- nvim-lua/nvim/init.lua
+++ nvim-lua/nvim/init.lua
@@ -32,6 +32,13 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "autocmds"
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.treesitter.start()
+  end
+})
+
 vim.schedule(function()
   require "mappings"
 end)
